a5889edbd547a1e1fc9bfc2a65c7ffddbe783f06
nishant-ctrl <knishant9135@gmail.com>
2026-07-29 17:45:51 +0530

    add auth str

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import proxy from 'express-http-proxy';
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -9,6 +10,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, Gateway!' });
 });
+app.use("/auth",proxy(process.env.AUTH_SERVICE))
+
 
 app.listen(PORT, () => {
   console.log(`Gateway is running on port ${PORT}`);
---
+++ backend/services/auth/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {connectDb} from './config/db.js';
+dotenv.config();
+
+const PORT = process.env.PORT || 8001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from Auth!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Auth running on port ${PORT}`);
+  connectDb();
+});
