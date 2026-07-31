59b70d6dcd6c062e2ba36374f901f485bb594bcf
nishant-ctrl <knishant9135@gmail.com>
2026-07-31 23:02:08 +0530

    update

--- backend/services/auth/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-import { getAuth } from "firebase-admin/auth"
-const login=async (req,res) => {
-    try {
-        const {token}=req.body;
-        await getAuth()
-    } catch (error) {
-        
-    }
-}
