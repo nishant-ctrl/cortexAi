35109387d01cbcf342e598d6b77d2a5b905cf6fb
nishant-ctrl <knishant9135@gmail.com>
2026-08-20 19:58:06 +0530

    add ppt agent

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,1 +1,76 @@
-export const pptAgent = async (params) => {};
+import { getModel } from "../config/llmModel.js";
+import { generatePpt } from "../utils/generatePpt.js";
+import { getFromS3 } from "../utils/getFromS3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
+
+export const pptAgent = async (state) => {
+    try {
+        const llm = await getModel("ppt");
+        const prompt = `You are a professional presentation designer.
+
+Return ONLY valid JSON.
+
+Format:
+
+{
+"title":"",
+"subtitle":"",
+"slides":[
+{
+"title":"",
+"points":[
+"",
+"",
+"",
+""
+]
+}
+]
+}
+
+Rules:
+
+- Generate exactly 6 content slides.
+- Each slide should have 4-6 concise bullet points.
+- No markdown.
+- No explanation.
+- No code block.
+- Return ONLY JSON.
+
+Topic:
+
+${state.prompt}`;
+
+        const res = await llm.invoke(prompt);
+        const data = JSON.parse(res.content);
+        const ppt = await generatePpt(data);
+        const buffer = await ppt.write({
+            outputType: "nodebuffer",
+        });
+        const filename = `ppt-${Date.now()}.pptx`;
+        await uploadToS3(
+            filename,
+            buffer,
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        );
+        const downloadUrl = await getFromS3(filename, 60 * 10);
+        return {
+            ...state,
+            aiResponse: `
+
+# ✅ Presentation Generated
+
+**${data.title}**
+
+📥 [Download PPT](${downloadUrl})
+
+_Link expires in 10 minutes._`,
+        };
+    } catch (error) {
+        console.log(error.message);
+        return {
+            ...state,
+            aiResponse: "Failed to generate ppt.",
+        };
+    }
+};
